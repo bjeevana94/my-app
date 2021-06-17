a1b29b67930d5923b0b31ab9a8fe084b5fcc15cc
jbommannagari <jeevana.bommannagari@afficienta.com>
2021-06-17 12:59:58 -0700

    task updated

---
+++ react-coding-task/src/Container/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// api : https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole
+
+// Task 1: fetch results from API
+
+// Task 2: show fetched results in the table
+
+// Task 3: Filter table results by created year (you can use slider or dopdown to filter)
+
+export function Main () {
+    return (
+        <div>
+            Happy Coding!
+        </div>
+    )
+}
